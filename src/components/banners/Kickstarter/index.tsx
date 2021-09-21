2dfa233697a6fdcae06e8e2bd6ebecd91528a990
T. Michael Keesey <keesey@gmail.com>
2021-09-21 14:13:15 -0700

    Revised KS banner

--- src/components/banners/Kickstarter/index.tsx
+++ src/components/banners/Kickstarter/index.tsx
@@ -1,23 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
-import { VFC } from "react"
-import ComicText from "~/components/ComicText"
+import { FC } from "react"
 import Logo from "~/components/Logo"
-import ThemeContext from "~/themes/ThemeContext"
 import styles from "./index.module.scss"
 export interface Props {
-    phase: "ending_soon" | "just_funded" | "just_launched" | "nearly_funded"
     url: string
 }
-const TEXT: Readonly<Record<Props["phase"], string>> = {
-    ending_soon: "Ending Soon",
-    just_funded: "Just Funded",
-    just_launched: "Just Launched",
-    nearly_funded: "Nearly Funded",
-}
-const Kickstarter: VFC<Props> = ({ phase, url }) => (
+const Kickstarter: FC<Props> = ({ children, url }) => (
     <a className={styles.container} href={url} role="button">
         <aside className={styles.banner}>
-            <span className={styles.phase}>{TEXT[phase]}</span>
+            <span className={styles.phase}>{children}</span>
             <cite>
                 <Logo color="white" type="paleocene" height={48} width={133} />
                 #3
