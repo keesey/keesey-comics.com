717562662c877897a54df2794155fa834d02cb7f
T. Michael Keesey <keesey@gmail.com>
2021-09-21 14:25:18 -0700

    Controlling Kickstarter banner with env var.

--- src/components/banners/CurrentBanner.tsx
+++ src/components/banners/CurrentBanner.tsx
@@ -1,5 +1,6 @@
 import { VFC } from "react";
 import Kickstarter from "./Kickstarter";
-const CurrentBanner: VFC = () =>
-    <Kickstarter url="https://www.kickstarter.com/projects/keesey/paleocene-3-comic-book">Coming soon</Kickstarter>
+const CurrentBanner: VFC = () => process.env.NEXT_PUBLIC_KICKSTARTER_PHASE
+    ? <Kickstarter url="https://www.kickstarter.com/projects/keesey/paleocene-3-comic-book">{process.env.NEXT_PUBLIC_KICKSTARTER_PHASE}</Kickstarter>
+    : null
 export default CurrentBanner
