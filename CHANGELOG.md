1b6c4f8abe0b9e44d45fb4fcf988721f279994dc
T. Michael Keesey <keesey@gmail.com>
2021-10-08 21:58:14 -0700

    Added countdown banner.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,6 +18,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Security
 
+## [2.0.7] - 2021-10-08
+
+### Added
+
+- Countdown banner for Kickstarter campaign.
+
 ## [2.0.6] - 2021-10-07
 
 ### Security
--- src/components/banners/CurrentBanner.tsx
+++ src/components/banners/CurrentBanner.tsx
@@ -1,6 +1,51 @@
-import { VFC } from "react";
-import Kickstarter from "./Kickstarter";
-const CurrentBanner: VFC = () => process.env.NEXT_PUBLIC_KICKSTARTER_PHASE
-    ? <Kickstarter url="//www.kickstarter.com/projects/keesey/paleocene-3-comic-book">{process.env.NEXT_PUBLIC_KICKSTARTER_PHASE}</Kickstarter>
-    : null
+import { useEffect, useState, VFC } from "react"
+import Kickstarter from "./Kickstarter"
+const END = process.env.NEXT_PUBLIC_KICKSTARTER_END_DATE ? new Date(process.env.NEXT_PUBLIC_KICKSTARTER_END_DATE).valueOf() : NaN
+const getCountdown = () => {
+    const now = new Date().valueOf()
+    const duration = END - now
+    if (duration < 1000) {
+        return null
+    }
+    if (duration < 60 * 1000) {
+        const seconds = Math.floor(duration / 1000)
+        return `${seconds} second${seconds === 1 ? "" : "s"} left!`
+    }
+    if (duration < 60 * 60 * 1000) {
+        const minutes = Math.floor(duration / (60 * 1000))
+        return `${minutes} minute${minutes === 1 ? "" : "s"} left!`
+    }
+    if (duration < 24 * 60 * 60 * 1000) {
+        const hours = Math.floor(duration / (60 * 60 * 1000))
+        return `${hours} minute${hours === 1 ? "" : "s"} left!`
+    }
+    {
+        const days = Math.floor(duration / (24 * 60 * 60 * 1000))
+        if (days % 7 === 0) {
+            const weeks = Math.round(days / 7)
+            return `${weeks} week${weeks === 1 ? "" : "s"} left`
+        }
+        return `${days} day${days === 1 ? "" : "s"} left`
+    }
+}
+const Message: VFC = () => {
+    if (process.env.NEXT_PUBLIC_KICKSTARTER_PHASE) {
+        return <Kickstarter url="//www.kickstarter.com/projects/keesey/paleocene-3-comic-book">{process.env.NEXT_PUBLIC_KICKSTARTER_PHASE}</Kickstarter>
+    }
+    return null
+}
+const Countdown: VFC = () => {
+    const [countdown, setCountdown] = useState(getCountdown)
+    useEffect(
+        () => {
+            const handle = setInterval(() => setCountdown(getCountdown()), 1000)
+            return () => clearInterval(handle)
+        },
+        [setCountdown],
+    )
+    return countdown
+        ? <Kickstarter url="//www.kickstarter.com/projects/keesey/paleocene-3-comic-book">{countdown}</Kickstarter>
+        : <Message />
+}
+const CurrentBanner: VFC = () => isNaN(END) ? <Message /> : <Countdown />
 export default CurrentBanner
