fca59800e7cd04b2ebc76123c7b23b61d6f53f61
T. Michael Keesey <keesey@gmail.com>
2022-01-05 23:55:00 -0800

    Cleanup

--- src/components/store/Invoice/Document.tsx
+++ src/components/store/Invoice/Document.tsx
@@ -7,24 +7,20 @@ export interface Props {
     approval?: State;
 }
 const Document: VFC<Props> = ({ approval }) => {
-    const [css, setCSS] = useState("")
-    useEffect(
-        () => {
-            if (approval) {
-                let newCSS = "";
-                for (const styleSheet of window?.top?.document?.styleSheets ?? []) {
-                    if (styleSheet.cssRules) {
-                        for (const cssRule of styleSheet.cssRules) {
-                            newCSS += cssRule.cssText;
-                        }
+    const [css, setCSS] = useState("");
+    useEffect(() => {
+        if (approval) {
+            let newCSS = "";
+            for (const styleSheet of window?.top?.document?.styleSheets ?? []) {
+                if (styleSheet.cssRules) {
+                    for (const cssRule of styleSheet.cssRules) {
+                        newCSS += cssRule.cssText;
                     }
                 }
-                console.log("NEW CSS", newCSS);
-                setCSS(newCSS);
             }
-        },
-        [approval],
-    );
+            setCSS(newCSS);
+        }
+    }, [approval]);
     if (!approval) {
         return null;
     }
@@ -38,7 +34,9 @@ const Document: VFC<Props> = ({ approval }) => {
                 <main className={styles.main}>
                     <header>
                         <h1>Keesey Comics: Order #{approval.data.orderID}</h1>
-                        <time dateTime={approval.response.create_time}>{new Date(approval.response.create_time).toString()}</time>
+                        <time dateTime={approval.response.create_time}>
+                            {new Date(approval.response.create_time).toString()}
+                        </time>
                     </header>
                     <div>
                         {approval.response.purchase_units.map((unit, index) => (
@@ -47,16 +45,25 @@ const Document: VFC<Props> = ({ approval }) => {
                     </div>
                     <hr />
                     <footer className={styles.footer}>
-                        <img src="/images/logos/black/keesey-comics.svg" width={94} height={56} alt="Keesey Comics" />
+                        <img
+                            src="/images/logos/black/keesey-comics.svg"
+                            width={94}
+                            height={56}
+                            alt="Keesey Comics"
+                        />
                         <div className={styles.contact}>
-                            <div>Keesey Comics</div>
+                            <div className={styles.title}>Keesey Comics</div>
                             <div>Los Angeles, California</div>
-                            <div><a href="mailto:keesey+comics@gmail.com">keesey+comics@gmail.com</a></div>
+                            <div>
+                                <a href="mailto:keesey+comics@gmail.com">
+                                    keesey+comics@gmail.com
+                                </a>
+                            </div>
                         </div>
                     </footer>
                 </main>
             </body>
         </html>
-    )
-}
+    );
+};
 export default Document;
