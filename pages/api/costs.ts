7938d7507c33cfc1d94da98b6ccc31d06fa60e50
Mike Keesey <keesey@gmail.com>
2022-02-22 19:10:16 -0800

    Fixed a build error with api/costs.

--- pages/api/costs.ts
+++ pages/api/costs.ts
@@ -1,17 +1,19 @@
 import { NextApiHandler } from "next"
 import calculateCosts from "~/cart/functions/calculateCosts"
 import { Address } from "~/cart/models/Address"
+import { Costs } from "~/cart/models/Costs"
 import { Order } from "~/cart/models/Order"
-const costs: NextApiHandler = async (req, res) => {
+const costs: NextApiHandler<Costs | string | void> = async (req, res) => {
     if (req.method !== "POST") {
-        return res.status(405).end()
-    }
-    try {
-        const data: { order: Order; address: Address } = req.body
-        const costs = await calculateCosts(data.order, data.address)
-        res.json(costs)
-    } catch (e) {
-        res.status(500).send(String(e))
+        res.status(405)
+    } else {
+        try {
+            const data: { order: Order; address: Address } = req.body
+            const costs = await calculateCosts(data.order, data.address)
+            res.json(costs)
+        } catch (e) {
+            res.status(500).send(String(e))
+        }
     }
 }
 export default costs
