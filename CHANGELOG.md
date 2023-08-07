ce9ea4d916e3d4c2463ababa56ce314d913cd647
Mike Keesey <keesey@gmail.com>
2023-08-07 12:50:13 -0700

    v2.3.7
    ### Changed
    
    -   Kickstarter link to Created Project tab in profile.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Security
 
+## [2.3.7] - 2023-08-07
+
+### Changed
+
+-   Kickstarter link to Created Project tab in profile.
+
 ## [2.3.6] - 2023-08-07
 
 ### Changed
--- pages/kickstarter/index.tsx
+++ pages/kickstarter/index.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next"
 import RedirectPage from "~/components/RedirectPage"
 const Redirect: NextPage = () => (
-    <RedirectPage href="//www.kickstarter.com/projects/keesey" title="Kickstarter" />
+    <RedirectPage href="//www.kickstarter.com/profile/keesey/created" title="Kickstarter" />
 )
 export default Redirect
