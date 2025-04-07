b2692b5fb3b2dd5921fbc9045388d9f127320bfe
Mike Keesey <keesey@gmail.com>
2025-04-07 12:34:31 -0700

    Revised Social Nav.

--- src/components/SocialNav/index.tsx
+++ src/components/SocialNav/index.tsx
@@ -7,14 +7,14 @@ const SocialNav: FC = () => {
     const logoColor = useForegroundColor()
     return (
         <Nav>
+            <a href="//patreon.com/tmkeesey" rel="author" role="button">
+                <Logo color={logoColor} type="patreon-p" width={50} height={50} />
+            </a>
             <Link href="/kickstarter" legacyBehavior passHref>
                 <a role="button">
                     <Logo color={logoColor} type="kickstarter-k" width={50} height={50} />
                 </a>
             </Link>
-            <a href="//patreon.com/tmkeesey" rel="author" role="button">
-                <Logo color={logoColor} type="patreon-p" width={50} height={50} />
-            </a>
         </Nav>
     )
 }
